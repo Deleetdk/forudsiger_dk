71e15e0dbd31fe63d346ae47e12f251ab421b2f3
Deleetdk <emil@emilkirkegaard.dk>
2016-01-25 20:42:10 +0100

    tilføjet flere info til adminpanelet om Event og Prediction.

--- forudsiger/main/admin.py
+++ forudsiger/main/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 
 from .models import Prediction, Event
 
-admin.site.register(Event)
-admin.site.register(Prediction)
+
+## More colons in admin panel
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("id", 'description', 'pub_date', "last_date", "end_date", "prediction_type", "creator")
+
+class PredictionAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "event", "prediction", "date")
+
+#register models in admin
+admin.site.register(Event, EventAdmin)
+admin.site.register(Prediction, PredictionAdmin)
